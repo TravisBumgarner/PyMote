1e05b841794683a3fd9b5d9de11a2d4d25e4689c
Travis Bumgarner <travis.bumgarner@gmail.com>
2018-03-29 11:58:48 -0400

    Initial commit- added  Home View, eslint

---
+++ src/views/App/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import { Home } from '../index';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <Home />
+    );
+  }
+}
